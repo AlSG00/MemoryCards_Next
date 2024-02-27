246605d3fa62062cf7b0b8363310a5124fcd05a5
AlSG <alex.gub2013@gmail.com>
2024-02-27 20:29:45 +0700

    v1.3.25: Backup

---
+++ Assets/DifficultyLevelMenuHandler.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DifficultyLevelMenuHandler : MenuHandler
+{
+    private void OnEnable()
+    {
+        ;'ojf'
+    }
+
+    private void OnDisable()
+    {
+        asdf
+    }
+}
