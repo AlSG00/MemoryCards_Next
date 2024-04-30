bebdd5bf2c5e88facc86e16a8489f845d2a94f11
AlSG <alex.gub2013@gmail.com>
2024-04-30 11:38:10 +0700

    v1.4.24 upd: Working on sound for the rest of the scale items

---
+++ Assets/Scripts/CoinAudioSourcePlayer.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoinAudioSourcePlayer : MonoBehaviour
+{
+    [SerializeField] private AudioSource _audioSource;
+    [SerializeField] private float _minAudioPitch;
+    [SerializeField] private float _maxAudioPitch;
+    [SerializeField] private AudioClip[] coinAudioClipArray;
+
+    private void OnEnable()
+    {
+        MainMoneyView.UpdatingMainMoneyCounter += Play;
+    }
+
+    private void OnDisable()
+    {
+        MainMoneyView.UpdatingMainMoneyCounter -= Play;
+    }
+
+    int count = 0;
+    private void Play(int foo = 0)
+    {
+        _audioSource.pitch = Random.Range(_minAudioPitch, _maxAudioPitch);
+        AudioClip clipToPlay = coinAudioClipArray[Random.Range(0, coinAudioClipArray.Length)];
+        _audioSource.PlayOneShot(clipToPlay);
+    }
+}
